dbbf58f7b555bebe6e6ec0cccea98bd33d0fc82a
Bincy Narath <bincybaburajnarath@gmail.com>
2020-08-12 04:21:50 -0400

    Craiglist api call and webscraping are done

---
+++ urls_list.py
@@ -0,0 +1,3 @@
+executable_path = '/usr/local/bin/chromedriver'
+craig_tor_all = 'https://toronto.craigslist.org/search/apa?bundleDuplicates=1&availabilityMode=0&sale_date=all+dates'
+craig_tor_posted_today = 'https://toronto.craigslist.org/search/apa?postedToday=1&bundleDuplicates=1&availabilityMode=0&sale_date=all+dates'
